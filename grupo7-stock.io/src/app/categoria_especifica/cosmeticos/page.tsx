0dc7be9a7079b2f77961a2135b4776c04a335632
Giovanna <giovannaab19@gmail.com>
2025-12-06 13:33:37 -0300

    fix: cosmeticos

--- grupo7-stock.io/src/app/categoria_especifica/cosmeticos/page.tsx
+++ grupo7-stock.io/src/app/categoria_especifica/cosmeticos/page.tsx
@@ -154,106 +154,6 @@ export default function FeedPage() {
       </div>
 
       <div className="relative z-10 bg-[#F6F3E4] h-full pl-10 pt-10">
-        <div className="flex items-center justify-end pr-5 pb-5">
-          <div className="flex flex-col">
-            <BarraPesquisa
-              dadosOriginais={produtosOriginais}
-              setDadosFiltrados={setProdutos}
-              chave="nome"
-              placeholder="Buscar produtos..."
-            />
-            {/*AQUI OH COMEÇA*/}
-            <div className="relative  w-130  pb-5 pt-5 ">
-              <button
-                className="flex justify-between items-center w-full bg-white p-4 rounded-xl shadow-md border border-gray-200 text-base font-semibold text-[#982829]"
-                onClick={() => setIsFilterOpen(!isFilterOpen)}
-              >
-                Filtros
-                <FaAngleDown
-                  className={`transition-transform duration-300 ${
-                    isFilterOpen ? "rotate-180" : ""
-                  }`}
-                />
-              </button>
-
-              {isFilterOpen && (
-                <div className="absolute top-full left-0 w-full bg-white border border-gray-300 shadow-lg rounded-xl p-4 mt-2 z-50 space-y-3">
-                  <div>
-                    <label className="block text-sm font-semibold text-[#982829] mb-1">
-                      Preço Máximo:{" "}
-                      <span className="text-black">R$ {pendentePreco}</span>
-                    </label>
-                    <input
-                      type="range"
-                      min={0}
-                      max={precoMaximoReal}
-                      step={1}
-                      value={pendentePreco}
-                      onChange={(e) => setPendentePreco(Number(e.target.value))}
-                      className="w-full accent-[#982829]"
-                    />
-                  </div>
-
-                  <div>
-                    <label className="block text-sm font-semibold text-[#982829] mb-1">
-                      Avaliação
-                    </label>
-                    <select
-                      value={pendenteRating}
-                      onChange={(e) => setPendenteRating(e.target.value as any)}
-                      className="w-full border border-gray-300 rounded-lg p-2 text-sm text-black"
-                    >
-                      <option value="Nenhum">Padrão</option>
-                      <option value="Melhor">Melhor Avaliados</option>
-                      <option value="Pior">Pior Avaliados</option>
-                    </select>
-                  </div>
-
-                  <div>
-                    <label className="block text-sm font-semibold text-[#982829] mb-1">
-                      Ordenar por Adição
-                    </label>
-                    <select
-                      value={pendenteSort}
-                      onChange={(e) => setPendenteSort(e.target.value as any)}
-                      className="w-full border border-gray-300 rounded-lg p-2 text-sm text-black"
-                    >
-                      <option value="Nenhum">Padrão</option>
-                      <option value="Mais Recente">Mais Recente</option>
-                      <option value="Mais Antiga">Mais Antiga</option>
-                    </select>
-                  </div>
-
-                  <button
-                    className="w-full bg-[#982829] text-white font-semibold py-1.5 px-3 rounded-lg hover:scale-105 transition text-sm"
-                    onClick={() => {
-                      aplicarFiltros();
-                      setIsFilterOpen(false);
-                    }}
-                  >
-                    Aplicar Filtros
-                  </button>
-
-                  <button
-                    className="w-full bg-gray-300 text-black font-semibold py-1.5 px-3 rounded-lg hover:scale-105 transition text-sm"
-                    onClick={() => {
-                      setPendentePreco(precoMaximoReal);
-                      setPendenteRating("Nenhum");
-                      setPendenteSort("Nenhum");
-
-                      setPrecoMaximo(precoMaximoReal);
-                      setRatingSort("Nenhum");
-                      setSortType("Nenhum");
-
-                      setCurrentPage(1);
-                      setIsFilterOpen(false);
-                    }}
-                  >
-                    Limpar Filtros
-                  </button>
-                </div>
-              )}
-            </div>
 
             <div className="relative z-10 bg-[#F6F3E4] h-300 pl-10 pt-10">
                 <div className="flex items-center justify-end pr-5 pb-5">
@@ -433,6 +333,7 @@ export default function FeedPage() {
                     )}
                 </div>
             </div>
+        </div>
         </>
     );
 }
